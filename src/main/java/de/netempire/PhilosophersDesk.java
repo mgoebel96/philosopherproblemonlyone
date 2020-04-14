74b98ca8799c5a2b6d86cf749b15426d021a0387
m_goebel <murat.goebel@googlemail.com>
2020-04-14 16:22:46 +0200

    16:15 140420

--- src/main/java/de/netempire/PhilosophersDesk.java
+++ src/main/java/de/netempire/PhilosophersDesk.java
@@ -14,45 +14,42 @@ import java.util.concurrent.TimeUnit;
 public class PhilosophersDesk {
 
     public static Semaphore eatingPhilosopher = new Semaphore(1, true);
+    static Fork fork1 = new Fork();
+    static Fork fork2 = new Fork();
+    static Fork fork3 = new Fork();
+    static Fork fork4 = new Fork();
+    static Fork fork5 = new Fork();
+    static Philosopher platon = new Philosopher("Platon", fork1, fork2);
+    static Philosopher aristoteles = new Philosopher("Aristoteles",fork2, fork3);
+    static Philosopher herder = new Philosopher("Herder", fork3, fork4);
+    static Philosopher fichte = new Philosopher("Fichte", fork4, fork5);
+    static Philosopher schlegel = new Philosopher("Schlegel", fork5, fork1);
+    static Thread platonThread = new Thread(platon);
+    static Thread aristotelesThread = new Thread(aristoteles);
+    static Thread schlegelThread = new Thread(schlegel);
+    static Thread fichteThread = new Thread(fichte);
+    static Thread herderThread = new Thread(herder);
+    static Date start = Calendar.getInstance().getTime();
+    static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    static Thread task2Thread = new Thread();
 
     public static void main(String[] args) {
         startProcess();
     }
 
     private static void startProcess() {
-        Date start = Calendar.getInstance().getTime();
+        initialize();
+        start();
+    }
 
-        Fork fork1 = new Fork();
-        Fork fork2 = new Fork();
-        Fork fork3 = new Fork();
-        Fork fork4 = new Fork();
-        Fork fork5 = new Fork();
-
-        Philosopher platon = new Philosopher("Platon", fork1, fork2);
+    private static void initialize() {
         platon.setEatingTime(750);
-        Philosopher aristoteles = new Philosopher("Aristoteles",fork2, fork3);
         aristoteles.setEatingTime(1000);
-        Philosopher herder = new Philosopher("Herder", fork3, fork4);
         herder.setEatingTime(300);
-        Philosopher fichte = new Philosopher("Fichte", fork4, fork5);
         fichte.setEatingTime(1500);
-        Philosopher schlegel = new Philosopher("Schlegel", fork5, fork1);
         schlegel.setEatingTime(500);
-
-        Thread platonThread = new Thread(platon);
-        platonThread.start();
-        Thread aristotelesThread = new Thread(aristoteles);
-        aristotelesThread.start();
-        Thread schlegelThread = new Thread(schlegel);
-        schlegelThread.start();
-        Thread fichteThread = new Thread(fichte);
-        fichteThread.start();
-        Thread herderThread = new Thread(herder);
-        herderThread.start();
-        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-
         Runnable controller = () -> {
-            if(!platonThread.isAlive() && !herderThread.isAlive() && !aristotelesThread.isAlive() && !fichteThread.isAlive() && !schlegelThread.isAlive()){
+            if (!platonThread.isAlive() && !herderThread.isAlive() && !aristotelesThread.isAlive() && !fichteThread.isAlive() && !schlegelThread.isAlive()) {
                 platon.stop();
                 herder.stop();
                 platon.stop();
@@ -64,7 +61,15 @@ public class PhilosophersDesk {
             }
         };
         executor.scheduleAtFixedRate(controller, 0, 4, TimeUnit.SECONDS);
-        Thread task2Thread = new Thread(controller);
+        task2Thread = new Thread(controller);
+    }
+
+    public static void start(){
+        platonThread.start();
+        aristotelesThread.start();
+        schlegelThread.start();
+        fichteThread.start();
+        herderThread.start();
         task2Thread.start();
     }
 
@@ -72,4 +77,5 @@ public class PhilosophersDesk {
         long difference = from.getTime() - to.getTime();
         return (int) (difference/1000);
     }
+
 }
